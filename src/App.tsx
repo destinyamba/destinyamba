6d1b034aa7b747deff06a6ddd64159b1dec8265f
destinyamba <destiny.amba@gmail.com>
2024-04-21 01:29:49 +0100

    remove redundant import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Chart from "./components/Charts/Chart";
 import DashboardHeading from "./components/Dashboard/DashboardHeading";
 import { Stack } from "@mui/material";
 import AddAmountForm from "./components/Form/AddAmountForm";
-import { useEffect } from "react";
 
 function App() {
   return (
